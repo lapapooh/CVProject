df774af2b978d1e3391aaa93c983b15419af5831
abdumalik2014 <abdumalik2014@gmail.com>
2019-03-26 13:43:55 +0500

    new VM

---
+++ INTRANET/ViewModels/HrEmployeeDocumentVM.cs
@@ -0,0 +1,15 @@
+﻿using INTRANET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INTRANET.ViewModels
+{
+    public class HrEmployeeDocumentVM
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public List<HrEmployeeDocument> HrEmployeeDocuments { get; set; }
+    }
+}
