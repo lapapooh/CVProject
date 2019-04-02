1e0021b6114feee555b87155910c643b92f4c282
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2019-04-02 12:19:52 +0500

    minor fix for doc upload

--- INTRANET/Controllers/HrEmployeeDocumentController.cs
+++ INTRANET/Controllers/HrEmployeeDocumentController.cs
@@ -99,7 +99,7 @@ namespace INTRANET.Controllers
                 model.FileName = fileItem.FileName;
                 model.FileContentType = fileItem.ContentType;
                 model.EmployeeId = employeeId;
-                model.Title = documentTitle;
+                model.Title = documentTitle ?? fileItem.FileName; //or will give arror if user did not type title
 
                 _hrEmployeeDocumentService.Create(model);
 
