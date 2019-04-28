b322b06507090681420ce1eccb9d3142c7ebf307
Durdona <durduw.b@gmail.com>
2019-04-28 18:06:45 +0500

    view models

---
+++ INTRANET/Models/HrCvLaborVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INTRANET.Models
+{
+    public class HrCvLaborVM
+    {
+        public string Years { get; set; }
+        public string Description { get; set; }
+    }
+}
