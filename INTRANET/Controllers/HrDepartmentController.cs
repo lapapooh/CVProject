a2bf5b58afd609734694067c2ae1670a5aa7f4d1
abdumalik2014 <abdumalik2014@gmail.com>
2019-05-07 18:24:13 +0500

    Demo push. please ignore. lost my changes while pulling

---
+++ INTRANET/Controllers/HrDepartmentController.cs
@@ -0,0 +1,79 @@
+﻿using INTRANET.Model;
+using INTRANET.Models;
+using INTRANET.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INTRANET.Controllers
+{
+    public class HrDepartmentController : Controller
+    {
+        IHrDepartmentService _hrDepartmentService { get; set; }
+
+        public HrDepartmentController(IHrDepartmentService hrDepartmentService)
+        {
+            _hrDepartmentService = hrDepartmentService;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _hrDepartmentService.GetAll().Select(MapTo).ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View(new HrDepartmentVM());
+        }
+
+        [HttpPost]
+        public ActionResult Create(HrDepartmentVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _hrDepartmentService.Create(MapFrom(model));
+                return RedirectToAction("Index");
+            }
+            else
+                return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(MapTo(_hrDepartmentService.GetByID(id)));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(HrDepartmentVM model)
+        {
+            _hrDepartmentService.Update(MapFrom(model));
+            return RedirectToAction("Index");
+        }
+
+        public HrDepartment MapFrom(HrDepartmentVM model)
+        {
+            return new HrDepartment
+            {
+                Code_1C = model.Code_1C,
+                TitleRu = model.TitleRu,
+                TitleUz = model.TitleUz,
+                TitleEn = model.TitleEn
+            };
+        }
+        public HrDepartmentVM MapTo(HrDepartment model)
+        {
+            return new HrDepartmentVM
+            {
+                Code_1C = model.Code_1C,
+                TitleRu = model.TitleRu,
+                TitleUz = model.TitleUz,
+                TitleEn = model.TitleEn
+            };
+        }
+
+    }
+}
---
+++ INTRANET/Models/HrDepartmentVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INTRANET.Models
+{
+    public class HrDepartmentVM
+    {
+        public string Code_1C { get; set; }
+        public string TitleRu { get; set; }
+        public string TitleUz { get; set; }
+        public string TitleEn { get; set; }
+    }
+}
