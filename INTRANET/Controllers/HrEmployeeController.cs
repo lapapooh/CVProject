6c1a9c76afcfb66c0696c33d57b226b010b4e565
abdumalik2014 <abdumalik2014@gmail.com>
2019-05-13 19:39:48 +0500

    HrEmployee CRUD implemented

---
+++ INTRANET/Controllers/HrEmployeeController.cs
@@ -0,0 +1,146 @@
+﻿using INTRANET.Model;
+using INTRANET.Models;
+using INTRANET.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INTRANET.Controllers
+{
+    public class HrEmployeeController : Controller
+    {
+        IHrEmployeeService _hrEmployeeService { get; set; }
+        IHrDepartmentService _hrDepartmentService { get; set; }
+        IHrPositionService _hrPositionService { get; set; }
+        public HrEmployeeController(IHrEmployeeService hrEmployeeService, 
+            IHrDepartmentService hrDepartmentService, IHrPositionService hrPositionService)
+        {
+            _hrEmployeeService = hrEmployeeService;
+            _hrDepartmentService = hrDepartmentService;
+            _hrPositionService = hrPositionService;
+        }
+
+        // GET: HrEmployee
+        public ActionResult Index()
+        {
+            var model = _hrEmployeeService.GetAll().Select(MapTo).ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.departmentList = _hrDepartmentService
+                                    .GetAll()
+                                    .Select(a => new SelectListItem
+                                    {
+                                        Text = a.TitleEn,
+                                        Value = a.Id.ToString()
+                                    }).ToList();
+
+            ViewBag.positionsList = _hrPositionService
+                                    .GetAll()
+                                    .Select(a => new SelectListItem
+                                    {
+                                        Text = a.TitleEn,
+                                        Value = a.Id.ToString()
+                                    }).ToList();
+            return View(new HrEmployeeVM());
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(HrEmployeeVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                _hrEmployeeService.Create(MapFrom(model));
+                return RedirectToAction("Index");
+            }
+            else
+                return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(MapTo(_hrEmployeeService.GetByID(id)));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(HrEmployeeVM model)
+        {
+            _hrEmployeeService.Update(MapFrom(model));
+            return RedirectToAction("Index");
+        }   
+
+        public HrEmployeeVM MapTo(HrEmployee model)
+        {
+            var departmentsList = _hrDepartmentService
+                        .GetAll()
+                        .Select(a => new SelectListItem
+                        {
+                            Text = a.TitleEn,
+                            Value = a.Id.ToString()
+                        }).ToList();
+
+            var positionsList = _hrPositionService
+                                    .GetAll()
+                                    .Select(a => new SelectListItem
+                                    {
+                                        Text = a.TitleEn,
+                                        Value = a.Id.ToString()
+                                    }).ToList();
+            return new HrEmployeeVM
+            {
+                Id = model.Id,
+                FullName = model.FullName,
+                Code_1C = model.Code_1C,
+                ID_1C = model.ID_1C,
+                DateOfBirth = model.DateOfBirth,
+                PlaceOfBirth = model.PlaceOfBirth,
+                Gender = model.Gender,
+                Address = model.Address,
+                EmailLogin = model.EmailLogin,
+                EntryDate = model.EntryDate,
+                LeaveDate = model.LeaveDate,
+                PositionId = model.PositionId,
+                DepartmentId = model.DepartmentId,
+                PositionStartDate = model.PositionStartDate,
+                IsActive = model.IsActive,
+                PassportNo = model.PassportNo,
+                PassportIssueDate = model.PassportIssueDate,
+                PassportIssuePlace = model.PassportIssuePlace,
+                Departments = departmentsList,
+                Positions = positionsList
+            };
+        }
+
+        public HrEmployee MapFrom(HrEmployeeVM model)
+        {
+            return new HrEmployee
+            {
+                Id = model.Id,
+                FullName = model.FullName,
+                Code_1C = model.Code_1C,
+                ID_1C = model.ID_1C,
+                DateOfBirth = model.DateOfBirth,
+                PlaceOfBirth = model.PlaceOfBirth,
+                Gender = model.Gender,
+                Address = model.Address,
+                EmailLogin = model.EmailLogin,
+                EntryDate = model.EntryDate,
+                LeaveDate = model.LeaveDate,
+                PositionId = model.PositionId,
+                DepartmentId = model.DepartmentId,
+                PositionStartDate = model.PositionStartDate,
+                IsActive = model.IsActive,
+                PassportNo = model.PassportNo,
+                PassportIssueDate = model.PassportIssueDate,
+                PassportIssuePlace = model.PassportIssuePlace
+            };
+        }
+    }
+}
---
+++ INTRANET/Models/HrEmployeeVM.cs
@@ -0,0 +1,64 @@
+﻿using INTRANET.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INTRANET.Models
+{
+    public class HrEmployeeVM
+    {
+     
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name cannot be empty")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Code from 1C cannot be empty")]
+        [DisplayName("Code from 1C")]
+        public string Code_1C { get; set; }
+        [Required]
+        public string ID_1C { get; set; }
+        [Required(ErrorMessage = "Date of birth cannot be empty")]
+        [DisplayName("Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Place of birth cannot be empty")]
+        [DisplayName("Place of birth")]
+        public string PlaceOfBirth { get; set; }
+        [Required(ErrorMessage = "Gender cannot be empty")]
+        [DisplayName("Gender")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "Address cannot be empty")]
+        [DisplayName("Address")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Email login cannot be empty")]
+        [DisplayName("Email login")]
+        public string EmailLogin { get; set; }
+        [Required(ErrorMessage = "Entry date cannot be empty")]
+        [DisplayName("Entry date")]
+        public DateTime EntryDate { get; set; }
+        [DisplayName("Leave date")]
+        public DateTime? LeaveDate { get; set; }
+        [DisplayName("Position Id")]
+        public int? PositionId { get; set; }
+        [DisplayName("Department Id")]
+        public int? DepartmentId { get; set; }
+        [DisplayName("Position start date")]
+        public DateTime? PositionStartDate { get; set; }
+        [DisplayName("Is Active")]
+        public bool IsActive { get; set; }
+        [DisplayName("Passport Number")]
+        public string PassportNo { get; set; }
+        [DisplayName("Passport Issue Date")]
+        public DateTime? PassportIssueDate { get; set; }
+        [DisplayName("Passport Issue Place")]
+        public string PassportIssuePlace { get; set; }
+
+        public List<SelectListItem> Departments { get; set; }
+        public List<SelectListItem> Positions { get; set; }
+
+    }
+}
